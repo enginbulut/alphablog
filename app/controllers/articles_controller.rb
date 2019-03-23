8ad7a20997dc76de29638803e3d7dc86fd1e0da5
Rails Udemy Course <enginbulut@live.com>
2019-03-23 15:10:49 +0300

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,6 +37,13 @@ class ArticlesController < ApplicationController
         @article=Article.find(params[:id])
     end
 
+    def destroy
+        @article=Article.find(params[:id])
+        @article.destroy
+        flash[:notice]= "Article was successfully deleted"
+        redirect_to articles_path
+    end
+
     private 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
