2d6c7bc14b0394066d493567151eb1f76b0bb513
Rails Udemy Course <enginbulut@live.com>
2019-03-25 20:29:59 +0300

    bug fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -60,5 +60,6 @@ class ArticlesController < ApplicationController
         if current_user !=  @article.user
             flash[:danger]="You can only edit or delete your own articles"
             redirect_to root_path
+        end
     end
 end
